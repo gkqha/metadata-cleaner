eb036b81b3221e9001e3a0bd3a8c2400f40fd2d6
gkqha <46457610+gkqha@users.noreply.github.com>
2022-04-17 16:47:16 +0800

    Update mdcleaner.py
    add word

--- mdcleaner.py
+++ mdcleaner.py
@@ -1,43 +1,56 @@
 import argparse
 import os
-import win32com.client as win32com    
+import win32com.client as win32com
 
 __author__ = "Istvan David"
 __license__ = "MIT"
 __version__ = "1.0.0"
 
 
-
-class MdCleaner():
-
+class MdCleaner:
     def run(self, rootFolder):
         print("Running cleaner on directory {}".format(rootFolder))
         for root, dirs, files in os.walk(rootFolder):
             path = root.split(os.sep)
             for file in files:
-                if file.endswith('.xlsx'):
-                    fileFullPath = root + '\\' + file
+                if file.endswith(".xlsx"):
+                    fileFullPath = root + "\\" + file
                     print(fileFullPath)
-                    
-                    excel = win32com.gencache.EnsureDispatch('Excel.Application')
+
+                    excel = win32com.gencache.EnsureDispatch("Excel.Application")
                     wb = excel.Workbooks.Open(fileFullPath, Local=True)
                     wb.RemovePersonalInformation = True
                     wb.Close(SaveChanges=1)
                     excel.Quit()
+                elif file.endswith(".docx"):
+                    fileFullPath = root + "\\" + file
+                    print(fileFullPath)
+
+                    word = win32com.gencache.EnsureDispatch("Word.Application")
+                    print(word)
+                    wb = word.Documents.Open(fileFullPath)
+                    wb.RemovePersonalInformation = True
+                    wb.Close()
+                    word.Quit()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-root",
-        default="c:",
-        help=("Provide root directory without trailing separator."
-              "Example '-root d:\my_folder'"
-              )
-        )
-        
+        default=str(os.getcwd),
+        help=(
+            "Provide root directory without trailing separator."
+            "Example '-root d:\my_folder'"
+        ),
+    )
+
     options = parser.parse_args()
-    rootFolder = options.root
-    
+    if options.root == "":
+        rootFolder = str(pathlib.Path.cwd())
+    else:
+        rootFolder = options.root
+
     print(rootFolder)
-    
+
     MdCleaner().run(rootFolder)
